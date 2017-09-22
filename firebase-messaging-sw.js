4ac698970fd61ef9e0e0032345f309df96bec625
Marcel F. Diogo <marcel@personaliza.com.br>
2017-09-21 23:06:33 -0300

    atualizacao9

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/4.1.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.1.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js");
 
 firebase.initializeApp({
   messagingSenderId: "781706800672"
@@ -8,5 +8,6 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function(payload) {
+  console.log("Received background message ", payload);
   return self.registration.showNotification({}, {});
 });
